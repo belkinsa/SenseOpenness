7ef99d21a7110b9dfbaae788a4a5e5d28d80e171
belkinsa <belkinsa@ubuntu.com>
2018-01-12 13:13:03 -0500

    Nikola auto commit.
    Nikola version: 7.8.11

--- conf.py
+++ conf.py
@@ -134,7 +134,7 @@ TRANSLATIONS_PATTERN = "{path}.{lang}.{ext}"
 
 NAVIGATION_LINKS = {
     DEFAULT_LANG: (
-	#("/about.html", "About"),
+	("output/pages/about/index.html", "About"),
         ("/archive.html", "Archive"),
         ("/categories/", "Tags"),
         ("/rss.xml", "RSS feed"),
