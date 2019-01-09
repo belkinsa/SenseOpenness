ab8be12b9eeadc364169c4f2f409142f65a08b5a
Svetlana Belkin <belkinsa@users.noreply.github.com>
2019-01-09 13:08:17 -0500

    Update conf.py

--- conf.py
+++ conf.py
@@ -136,7 +136,7 @@ NAVIGATION_LINKS = {
     DEFAULT_LANG: (
 	("blog", "Blog"),
 	("/categories/", "Categories"),
-    ("/wetlands/index.htlm", "Wetlands Index"),
+    ("/wetlands/", "Wetlands Index"),
     ("/programs-that-i-use/", "Progams Used"),
     ),
 }
